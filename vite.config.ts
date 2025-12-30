7da18c9efda99591b764e401a63e3a8ea4850b82
Edward <edwardtorres355@gmail.com>
2025-12-29 21:12:21 -0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked relatively for GitHub Pages
+  base: '/3d-chess-game/', // Ensures assets are linked relatively for GitHub Pages
 })
